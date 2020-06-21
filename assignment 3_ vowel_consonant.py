7af035c1ed444c4b214d0c2931eab89693bc2d2e
vennela2 <67159241+vennela2@users.noreply.github.com>
2020-06-21 11:05:35 +0530

    Add files via upload

---
+++ assignment 3_ vowel_consonant.py
@@ -0,0 +1,5 @@
+ch=input("please enter your own character:")
+if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+	print("the given character ",ch,"is a vowel")
+else:
+		print("the given character",ch,"is a consonant")
